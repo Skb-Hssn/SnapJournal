1f9212a19034eeb0c700450897c20900a414fe49
souroveskb <souroveskb@gmail.com>
2022-04-17 02:03:44 +0600

    Home Page

--- lib/SnapJournal/constants/enums.dart
+++ lib/SnapJournal/constants/enums.dart
@@ -1,1 +1,6 @@
 const backgroundColor = 0xFBFBFBFF;
+const VioletAccent = 0x7c58ff;
+const LightViolet = 0xe3e4f5;
+const SkyBlueAccent = 0xbed5ff;
+const PasteAccent = 0x9ff6ce;
+const Orangish = 0xfea383;
